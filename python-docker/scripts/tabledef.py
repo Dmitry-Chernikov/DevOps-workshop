de60c31f0ec5c8a639829a5699b4ba518e73c9cc
dmitry-chernikov <chernicov_dima@mail.ru>
2023-03-19 12:01:47 +0400

    Initial commit

---
+++ python-docker/scripts/tabledef.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+#import sqlalchemy
+#import flask_sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Local
+SQLALCHEMY_DATABASE_URI = 'sqlite:///accounts.db'
+
+# Heroku
+#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+Base = declarative_base()
+
+
+def db_connect():
+    """
+    Performs database connection using database settings from settings.py.
+    Returns sqlalchemy engine instance
+    """
+    #Добавил echo=True все операции с бд будут логгироваться на консоль
+    return create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), unique=True)
+    password = Column(String(512))
+    email = Column(String(50))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+engine = db_connect()  # Connect to database
+Base.metadata.create_all(engine)  # Create models
